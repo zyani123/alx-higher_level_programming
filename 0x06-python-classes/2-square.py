6ab0e62e8f41281820ac1306106c25c2c85937ef
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-06-28 19:39:36 +0100

    update

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines the square class"""
+
+
+class Square:
+     def __init__(self, size=0):
+        """Initialize a new Square.
+
+
+        Args:
+        size (int): The size of the new square.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
