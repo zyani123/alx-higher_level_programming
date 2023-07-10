2def68261c02d402ddecf4ea08a5d13f0e2ea2c4
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-07-10 19:11:58 +0100

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Defines a Rectangle subclass Square."""
-Rectangle = __import__('9-rectangle').Rectangle
+"""Defines a class MyInt that inherits from int."""
 
 
-class Square(Rectangle):
-    """Represent a square."""
+class MyInt(int):
+    """Invert int operators == and !=."""
 
-    def __init__(self, size):
-        """Initialize a new square.
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
-        Args:
-            size (int): The size of the new square.
-        """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
-        self.__size = size
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
