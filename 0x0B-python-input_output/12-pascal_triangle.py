08e4211157f15e0c3dd200fc1dbd9f1feb4b9080
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-07-12 02:47:58 +0100

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 """pascal_triangle module.
 
-Contains a function that appends a text file.
+Contains a function that returns a list of lists of
+integers representing the Pascal’s triangle of n.
 """
 
-if __name__ == "__main__":
 
-    def pascal_triangle(n):
-        list1 = []
-        for i in range(1, n + 1):
-            for j in range(1, i + 1):
-                list1.append(j)
-        return list1
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers
+    representing the Pascal’s triangle of n.
+    """
+    if n <= 0:
+        return []
+
+    result = [[1]]
+    while len(result) is not n:
+        tmp = [1]
+        for i in range(len(result[-1]) - 1):
+            tmp.append(result[-1][i] + result[-1][i + 1])
+        tmp.append(1)
+        result.append(tmp)
+    return result
