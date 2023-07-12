03bb5d18a0fef8afcfc10898204bc69a8c3a6bd0
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-07-12 02:43:12 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,9 +5,11 @@
 Contains a function that writes a text file.
 """
 
-def write_file(filename="", text=""):
-    """function that writes a string to a text file and return the number
-    of characters written"""
 
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and
+    returns the number of characters written.
+    """
     with open(filename, 'w') as f:
         return f.write(text)
