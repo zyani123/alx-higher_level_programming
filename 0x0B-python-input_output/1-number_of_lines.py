d94d97fb9d7e8fa2d514063bc0e98b61fc1a2278
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-07-12 02:34:37 +0100

    DELETE

--- 0x0B-python-input_output/1-number_of_lines.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def number_of_lines(filename=""):
-    """function that reads a text and return number of lines"""
-
-    lines = 0
-    with open(filename) as f:
-        for lin in f:
-            lines += 1
-        return (lines)
