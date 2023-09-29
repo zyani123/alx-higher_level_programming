e23958a803d27c5f1c1b93b94f82e314238a2c12
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-09-29 17:02:25 +0100

    Update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-/* script that prints 3 lines:
-    'C is fun'
-    'Python is cool'
-    'Javascript is amazing'
-*/
-
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
