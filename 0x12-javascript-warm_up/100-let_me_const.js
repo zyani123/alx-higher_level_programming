28301d2a16554d739053b770773b81851817a89a
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-10-11 11:45:56 +0100

    Delete

--- 0x12-javascript-warm_up/100-let_me_const.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-/*  file that modifies the value of myVar to 333
-*/
-
-myVar = 333;
-exports.myVar = myVar;
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const _strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < _strings.length; i++) {
-  console.log(_strings[i]);
-}
