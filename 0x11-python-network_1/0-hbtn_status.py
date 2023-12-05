a9686240b49746552a1de54c3b89c46e1bd20eaa
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-12-05 21:02:49 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Fetches a URL."""
-import urllib.request as request
+"""fetches https://alx-intranet.hbtn.io/status
+"""
 
 
 if __name__ == "__main__":
-    with request.urllib("https://intranet.hbtn.io/status") as response:
-        if response.readable():
-            data = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(data)))
-            print("\t- content: {}".format(data))
-            print("\t- utf8 content: {}".format(data.decode("utf-8")))
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
