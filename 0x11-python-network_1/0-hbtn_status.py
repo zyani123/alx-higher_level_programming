b844ca0dcffd01de71fa6fb52f7359f61c352d9b
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-12-05 21:00:01 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request as request
 
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
+    with request.urllib("https://intranet.hbtn.io/status") as response:
         if response.readable():
             data = response.read()
             print("Body response:")
